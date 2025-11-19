9c92e87393b74dbc12b9761fbe06038af7305eeb
jonicinco <jocabpe@hotmail.es>
2025-11-19 04:47:29 +0100

    Create audio.cpp

---
+++ firmware/src/audio.cpp
@@ -0,0 +1,12 @@
+// audio.cpp – Manejo del audio, I2S, DAC y generadores
+
+#include "audio.h"
+#include "config.h"
+
+void audio_init() {
+    // Inicializar bus I2S
+}
+
+void audio_update() {
+    // Mantener generadores, reproducir archivos, etc.
+}
