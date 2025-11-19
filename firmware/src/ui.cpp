a2407f723850364bb1a608f203263f1e0da1d3b7
jonicinco <jocabpe@hotmail.es>
2025-11-19 04:47:58 +0100

    Create ui.cpp

---
+++ firmware/src/ui.cpp
@@ -0,0 +1,15 @@
+#include "ui.h"
+#include "config.h"
+
+void ui_init() {
+    // Inicializar pantalla OLED
+    // Inicializar encoder, botones, LEDs
+}
+
+void ui_showSplash() {
+    // Mostrar logo inicial
+}
+
+void ui_update() {
+    // Leer encoder y actualizar menú
+}
