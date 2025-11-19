0e10443832a3cb0f47443a10780f1b4d72e834a1
jonicinco <jocabpe@hotmail.es>
2025-11-19 04:48:49 +0100

    Create config.h

---
+++ firmware/src/config.h
@@ -0,0 +1,24 @@
+#pragma once
+
+// ------------------ PINOUT GENERAL ------------------
+
+// Pines del OLED (I2C)
+#define OLED_SDA 21
+#define OLED_SCL 22
+
+// Encoder rotativo
+#define ENC_A 32
+#define ENC_B 33
+#define ENC_BTN 25
+
+// Botones extra
+#define BTN1 26
+#define BTN2 27
+
+// LEDs RGB
+#define LED_RGB 4
+
+// SD (SPI)
+#define SD_CS 5
+
+// Ajustar según versión de hardware!
